73aa670d3437102155e285bcdc2bc605f39b5dc6
bbaga05 <bashkov.astana12@icloud.com>
2025-05-20 20:32:41 +0500

    Решение

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,20 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+
+        System.out.println(bean1.getMessage());
+        System.out.println(bean2.getMessage());
+        System.out.println("Сравнение бинов hw: " + (bean1==bean2));
+
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(cat1.getName() );
+        System.out.println(cat2.getName() );
+        System.out.println("Сравнение бинов cat: " + (cat1==cat2));
+
+
     }
 }
